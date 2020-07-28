0ea96efedb44c44a24ee67590475b038d1701e06
sametatila <68744691+sametatila@users.noreply.github.com>
2020-07-28 14:13:36 +0300

    fsdfsdfsffd

---
+++ spreadshirt.com.py
@@ -0,0 +1,39 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+import time
+
+#Ah ney yapsam ne etsem oldurabilsem. Olmuyor Amk olmuyor
+
+#Normal variables
+login_url = "https://partner.spreadshirt.com/login"
+username1 = "sametatila@gmail.com"
+password = "Linkin.123"
+create_url = ""
+product_title = "Oha amk!"
+tags = "asd,asda,asdsad"
+description = "asdfsadfsdfsdfdf"
+design_path = "C:\ytw/az.png"
+bg_color = "#000000"
+
+#Run Chromium
+options = Options()
+options.binary_location = r"C:/ytw/browser/chrome.exe"
+options.add_argument("--mute-audio")
+driver = webdriver.Chrome(chrome_options=options)
+
+###Start
+#Login Page
+driver.get(login_url)
+time.sleep(5)
+
+#Login Info
+driver.find_element_by_xpath('').send_keys(username1)
+time.sleep(3)
+driver.find_element_by_xpath('').send_keys(password)
+time.sleep(3)
+
+#Login Button
+driver.find_element_by_xpath('').click()
+time.sleep(5)
+###End
